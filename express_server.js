e4c18ea683e37b1c27341feb679a4926ef77c22a
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-03-01 22:42:52 -0800

    Add generateRandomString

--- express_server.js
+++ express_server.js
@@ -7,6 +7,33 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// Create 6-length random string
+const generateRandomString = () => {
+
+  // a-z, A-Z, 0-9
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; 
+  let output = '';
+  let length = 0;
+
+  // Generate random string
+  while (length < 6) { 
+    let randomChar = characters[Math.floor(Math.random() * characters.length)];
+    output += randomChar;
+    length += 1;
+  }
+
+  const shortenedUrls = Object.keys(urlDatabase);
+
+  // Repeat string generation if string is already taken
+  if (shortenedUrls.includes(output)) { 
+    generateRandomString();
+  }
+
+  return output;
+};
+
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -22,6 +49,10 @@ app.get("/urls", (req, res) => {
   res.render('urls_index.ejs', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
@@ -35,6 +66,11 @@ app.get("/urls/:id", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
