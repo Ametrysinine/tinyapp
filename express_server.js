5bcb099ec2edfbb593c8ee190891f947dbdc4611
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-03-09 10:56:11 -0800

    Require url ownership to change/delete

--- express_server.js
+++ express_server.js
@@ -6,11 +6,24 @@ const PORT = 8080; // default port 8080
 const users = {};
 
 const urls = {
-  "b2xVn2": {longURL: "http://www.lighthouselabs.ca"},
-  "9sm5xK": {longURL: "http://www.google.com"},
+  "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "admin"},
+  "9sm5xK": {longURL: "http://www.google.com", userID: "admin"},
 };
 
 const shortenedUrls = Object.keys(urls);
+const urlsForUser = function(userID) {
+  const userUrls = {};
+
+  // Add url to userUrls if userID is a match, or public ID
+  for (let url in urls) {
+    if (urls[url].userID === userID || urls[url].userID === "public") {
+      userUrls[url] = urls[url];
+    }
+  }
+  console.log(userUrls);
+
+  return userUrls;
+};
 
 // Create 6-length random string
 const generateRandomString = () => {
@@ -29,7 +42,7 @@ const generateRandomString = () => {
 
 
 
-  // Repeat string generation if string is already taken TODO: If userIDs is taken
+  // Repeat string generation if string is already taken by urlID or userID
   if (shortenedUrls.includes(output) || users.hasOwnProperty(output)) {
     generateRandomString();
   }
@@ -70,9 +83,11 @@ app.get("/login", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const user = users[req.cookies.user_id];
+  const userUrls = urlsForUser(req.cookies.user_id);
+
   const templateVars = {
     user,
-    urls,
+    userUrls,
   };
 
   console.log(urls);
@@ -81,11 +96,10 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  if (!req.cookies.user_id) {
+  const user = req.cookies.user_id;
+  if (!user) {
     res.redirect("/login");
   }
-
-  const user = users[req.cookies.user_id];
 
   const templateVars = {
     user,
@@ -96,9 +110,18 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const user = users[req.cookies.user_id];
+  const user = req.cookies.user_id;
   const urlID = req.params.id;
   const url = urls[urlID];
+
+  if (!user) {
+    res.redirect("/login");
+  }
+
+  if (url.userID !== user) {
+    res.status(401).send("You are not authorized to view this page.");
+    return;
+  }
 
   const templateVars = {
     user,
@@ -111,6 +134,7 @@ app.get("/urls/:id", (req, res) => {
 
 // Redirect to shortened URL
 app.get("/u/:id", (req, res) => {
+
   const longURL = urls[req.params.id];
 
   if (longURL) {
@@ -183,10 +207,22 @@ app.post("/logout", (req, res) => {
 app.post("/urls/:id", (req, res) => {
   const userID = req.cookies.user_id;
   const urlID = req.params.id;
+  const url = urls[urlID];
+
   const longURL = req.body.longURL;
 
   if (!userID) {
     res.status(401).send("Error: not logged in, login at /login");
+    return;
+  }
+
+  if (url.userID !== userID) {
+    res.status(401).send("You are not authorized to change this.");
+    return;
+  }
+
+  if (!url) {
+    res.status(402).send("Invalid URL");
     return;
   }
 
@@ -195,12 +231,13 @@ app.post("/urls/:id", (req, res) => {
     return;
   }
 
-  // Update url database with new URL
+  // Update url database with changed URL
   urls[urlID] = {
     longURL,
     userID,
   };
 
+  console.log(urls);
   res.redirect(`/urls/`);
 });
 
@@ -220,9 +257,9 @@ app.post("/urls", (req, res) => {
   }
   // Add new URL to 'database'
   const urlID = generateRandomString();
-  urls[urlID] = {longURL,
-    userID,
-  };
+  const newUrl = {longURL, userID};
+
+  urls[urlID] = newUrl;
 
   // Redirect to shortened URL
   res.redirect(`/urls/${urlID}`);
@@ -230,11 +267,24 @@ app.post("/urls", (req, res) => {
 
 app.post("/urls/:id/delete", (req, res) => {
   const userID = req.cookies.user_id;
+  const urlID = req.params.id;
+  const url = urls[urlID];
 
   if (!userID) {
     res.status(401).send("Error: not logged in, login at /login");
     return;
   }
+
+  if (url.userID !== userID) {
+    res.status(401).send("You are not authorized to delete this.");
+    return;
+  }
+
+  if (!url) {
+    res.status(402).send("Invalid URL");
+    return;
+  }
+
   delete urls[req.params.id];
 
   res.redirect("/urls/");
